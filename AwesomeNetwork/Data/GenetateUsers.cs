9e1320637de79bcb9e3f8990636ced10a57bb6a4
apocatastas <molchanova.a.a@gmail.com>
2025-05-03 17:04:00 +0300

    Add project files.

---
+++ AwesomeNetwork/Data/GenetateUsers.cs
@@ -0,0 +1,53 @@
+﻿using AwesomeNetwork.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.Data
+{
+    public class GenetateUsers
+    {
+        public readonly string[] maleNames = new string[] { "Aidan", "Khalid", "Ash", "Nimus", "Pair", "Christian", "Straker", "Vokhial", "Moandor", "Ajith" };
+        public readonly string[] femaleNames = new string[] { "Aeris", "Giselle", "Melodie", "Charna", "Valeska", "Vedomina", "Sinxa" };
+        public readonly string[] lastNames = new string[] { "Inferno", "Castle", "Nekropolis", "Citadel", "Tower" };
+
+        public List<User> Populate(int count)
+        {
+            var users = new List<User>();
+            for (int i = 1; i < count; i++)
+            {
+                string firstName;
+                var rand = new Random();
+
+                var male = rand.Next(1, 2) == 1;
+
+                var lastName = lastNames[rand.Next(0, lastNames.Length - 1)];
+                if (male)
+                {
+                    firstName = maleNames[rand.Next(0, maleNames.Length - 1)];
+                }
+                else
+                {
+                    lastName = lastName + "a";
+                    firstName = femaleNames[rand.Next(0, femaleNames.Length - 1)];
+                }
+
+                var item = new User()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    BirthDate = DateTime.Now.AddDays(-rand.Next(1, (DateTime.Now - DateTime.Now.AddYears(-25)).Days)),
+                    Email = "hero" + rand.Next(0, 1204) + "@heroes.com",
+                };
+
+                item.UserName = item.Email;
+                item.Image = "https://apocatastas.github.io/thebestdeveloperever/dummy.png";
+
+                users.Add(item);
+            }
+
+            return users;
+        }
+    }
+}
---
+++ AwesomeNetwork/Data/Repository/FriendsRepository.cs
@@ -0,0 +1,72 @@
+﻿using AwesomeNetwork.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.Data.Repository
+{
+    public class FriendsRepository : Repository<Friend>
+    {
+        public FriendsRepository(ApplicationDbContext db)
+      : base(db)
+        {
+            
+        }
+        
+
+        public void AddFriend (User target, User Friend)
+        {
+            var friends = Set.AsEnumerable().FirstOrDefault(x => x.UserId == target.Id && x.CurrentFriendId == Friend.Id);
+
+            if (friends == null)
+            {
+                var item = new Friend()
+                {
+                    UserId = target.Id,
+                    User = target,
+                    CurrentFriend = Friend,
+                    CurrentFriendId = Friend.Id,
+                };
+
+                var item_mirrored = new Friend()
+                {
+                    UserId = Friend.Id,
+                    User = Friend,
+                    CurrentFriend = target,
+                    CurrentFriendId = target.Id,
+                };
+
+                Create(item);
+                Create(item_mirrored);
+            }
+        }
+
+        public List<User> GetFriendsByUser(User target)
+        {
+            var friends = Set.Include(x => x.CurrentFriend).AsEnumerable().Where(x => x.UserId == target.Id).Select(x => x.CurrentFriend);
+
+            return friends.ToList();
+        }
+
+
+        public void DeleteFriend(User target, User Friend)
+        {
+            var friends = Set.AsEnumerable().FirstOrDefault(x => x.UserId == target.Id && x.CurrentFriendId == Friend.Id);
+
+            if (friends != null)
+            {
+                Delete(friends);
+            }
+
+            var friends_mirrored = Set.AsEnumerable().FirstOrDefault(x => x.UserId == Friend.Id && x.CurrentFriendId == target.Id);
+
+            if (friends_mirrored != null)
+            {
+                Delete(friends_mirrored);
+            }
+        }
+
+    }
+}
